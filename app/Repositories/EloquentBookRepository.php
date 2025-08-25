825f3edd4f20c2824c4303a3506ab3573b95292b
Miguel Ramos <return.infox@gmail.com>
2025-08-25 05:17:49 -0400

    actualizacion en eloquent book repository

--- app/Repositories/EloquentBookRepository.php
+++ app/Repositories/EloquentBookRepository.php
@@ -13,7 +13,13 @@ class EloquentBookRepository implements BookRepository
 
     public function update(Book $book, array $data): Book
     {
+        // 1. Se actualiza la base de datos
         $book->update($data);
+
+        // 2. Se refresca el objeto $book desde la base de datos
+        $book->refresh();
+
+        // 3. Se devuelve el objeto ya con los datos nuevos ("Richard")
         return $book;
     }
 
